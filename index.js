aa8741c1f6683021028b62e9d00f8dd06070385b
Rahul  Ramakrishnan <rahulramakris@gmail.com>
2023-09-25 21:21:56 -0400

    Add password length selector

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let button = document.getElementById("button");
 let output1 = document.getElementById("output1");
 let output2 = document.getElementById("output2");
+let lengthTitle = document.getElementById("password-length-title")
+let passwordSlider = document.getElementById("password-length")
 
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passLength = 15
+let passLength = passwordSlider.value;
+lengthTitle.textContent = `Password Length: ${passLength}`
 
 function getRandomIndex() {
     return Math.floor(Math.random() * characters.length)
@@ -24,4 +27,9 @@ button.addEventListener("click", function() {
     output1.textContent = generatePassword()
     output2.textContent = generatePassword()
 })
+
+passwordSlider.oninput = function() {
+    passLength = passwordSlider.value;
+    lengthTitle.textContent = `Password Length: ${passLength}`
+}
 
